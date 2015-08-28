e47b4572c3d1a1c1f9b3fe4234e1370dacb31ed1
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-08-28 10:16:04 -0600

    Refactoring; moved to a class library

--- AnotherProject/ClassCreator.cs
+++ AnotherProject/ClassCreator.cs
@@ -1,25 +1,44 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Xml.XPath;
 
-namespace MetaSandbox
+namespace MetaProgrammer
 {
-    class Program
+    public class ClassCreator
     {
+        //private string ProjectName { get; set; }
 
-        private static string _baseDir = null;
-        private static string BaseDir
+        private string _projectName = null;
+        private string ProjectName
+        {
+            get
+            {
+                if (_projectName != null)
+                    return _projectName;
+
+                var fileName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+                var parts = fileName.Split('.');
+                _projectName = parts[0];
+
+                return _projectName;
+            }
+        }
+
+        private string _baseDir = null;
+        private string BaseDir
         {
             get
             {
                 if (_baseDir != null)
                     return _baseDir;
+
+                //var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+                //var index = baseDir.IndexOf(ProjectName, StringComparison.InvariantCulture);
+                //_baseDir = baseDir.Substring(0, index);
 
                 var baseDir = AppDomain.CurrentDomain.BaseDirectory;
                 var parts = baseDir.Split('\\');
@@ -37,23 +56,23 @@ namespace MetaSandbox
             }
         }
 
-        private static XDocument _xdoc = null;
-        private static XDocument XDoc
+        private XDocument _xdoc = null;
+        private XDocument XDoc
         {
             get
             {
                 if (_xdoc != null)
                     return _xdoc;
 
-                var fullPath = Path.Combine(BaseDir, "MetaSandbox.csproj");
+                var fullPath = Path.Combine(BaseDir, ProjectName + ".csproj");
                 _xdoc = XDocument.Load(fullPath);
 
                 return _xdoc;
             }
         }
 
-        private static XElement _compileItemGroup = null;
-        private static XElement CompileItemGroup
+        private XElement _compileItemGroup = null;
+        private XElement CompileItemGroup
         {
             get
             {
@@ -66,20 +85,15 @@ namespace MetaSandbox
             }
         }
 
-        static void Main(string[] args)
+        public void CreateClass(string classNamespace, string className)
         {
-            ProjectFileTest();
-        }
-
-        private static void ProjectFileTest()
-        {
-            var success = WriteClass("MetaSandbox", "TestClass");
+            var success = WriteClass(classNamespace, className);
 
             if (success)
-                AddClassToProject("TestClass");
+                AddClassToProject(className);
         }
 
-        private static bool WriteClass(string classNamespace, string className)
+        private bool WriteClass(string classNamespace, string className)
         {
             var classStr =
                 "using System;\nusing System.Collections.Generic;\nusing System.Linq;\nusing System.Text;\nusing System.Threading.Tasks;\n\nnamespace " +
@@ -96,12 +110,11 @@ namespace MetaSandbox
             return false;
         }
 
-        private static void AddClassToProject(string className)
+        private void AddClassToProject(string className)
         {
             var classElem = new XElement(XDoc.Root.GetDefaultNamespace() + "Compile", new XAttribute("Include", className + ".cs"));
             CompileItemGroup.Add(classElem);
-            XDoc.Save(Path.Combine(BaseDir, "MetaSandbox.csproj"));
+            XDoc.Save(Path.Combine(BaseDir, ProjectName + ".csproj"));
         }
-
     }
 }
---
+++ DifferentProjectNameThanSolution/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using MetaProgrammer;
+
+namespace DifferentProjectNameThanSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cc = new ClassCreator();
+            cc.CreateClass("Test", "TestClass");
+        }
+
+    }
+}
