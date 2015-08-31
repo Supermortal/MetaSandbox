81e9320320ffefd8bde73d1324deded1cd18d2f2
Christopher Pink <Christopher.Pink@extendhealth.com>
2015-08-31 09:03:57 -0600

    Added an enum

---
+++ MetaProgrammer/Models/Enums/PropertyDataType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaProgrammer.Models.Enums
+{
+    public enum PropertyDataType
+    {
+        String,
+        Double,
+        Integer,
+        DateTime,
+        Decimal
+    }
+}
